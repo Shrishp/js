ba900cef750c3ee910186e79059347073043c1bd
shrish Pattewar <147394036+Shrishp@users.noreply.github.com>
2024-08-20 17:15:08 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using nodejs not browser
+
+// code readability has to be good
+
+
+console.log(3+3)
+
+let name="shrish"
+let age =19
+let isLoggedIn=false
+
+//number => 2 to powe 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//symbol => unique
+
+//object
+
+console.log(typeof "shrish")//string
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object

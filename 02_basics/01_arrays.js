58d35fcfd38cbff73fb37e35717e4beb4bec1049
shrish Pattewar <147394036+Shrishp@users.noreply.github.com>
2024-10-18 16:44:16 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -15,13 +15,7 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.unshift(9)
 // myArr.shift()
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
-
-// console.log(myArr);
-// console.log( newArr);
 
 
 // slice, splice
